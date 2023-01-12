7bab7b8ae0543e7d7e67ea66bbaa646149786c7e
Kim Abcouwer <kim@aktivate.com>
2023-01-12 14:21:16 -0700

    Create ship class and tests

---
+++ spec/ship_spec.rb
@@ -0,0 +1,46 @@
+require_relative 'spec_helper'
+
+RSpec.describe Ship do
+    before :each do
+        @cruiser = Ship.new("Cruiser", 3)
+    end
+
+    describe 'instantiation' do
+        it 'exists' do
+            expect(@cruiser).to be_a(Ship)
+        end
+
+        it 'has readable attributes' do
+            expect(@cruiser.name).to eq("Cruiser")
+            expect(@cruiser.length).to eq(3)
+        end
+    end
+
+    describe '#health' do
+        it 'can output health of a ship' do
+            expect(@cruiser.health).to eq(3)
+        end
+    end
+
+    describe '#sunk?' do
+        it 'can output if a ship has been sunk or not' do
+            expect(@cruiser.sunk?).to eq(false)
+        end
+    end
+
+    describe '#hit' do
+        it 'can remove 1 health unit if ship is hit' do
+            @cruiser.hit
+            expect(@cruiser.health).to eq(2)
+            expect(@cruiser.sunk?).to eq(false)
+
+            @cruiser.hit
+            expect(@cruiser.health).to eq(1)
+            expect(@cruiser.sunk?).to eq(false)
+
+            @cruiser.hit
+            expect(@cruiser.health).to eq(0)
+            expect(@cruiser.sunk?).to eq(true)
+        end
+    end
+end
